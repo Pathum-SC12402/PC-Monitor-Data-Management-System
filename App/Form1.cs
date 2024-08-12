0a96c8a027db7c50939dea16bd34746bf0528f89
Pathum-SC12402 <fernando12402@usci.ruh.ac.lk>
2024-08-13 00:50:28 +0530

    add

---
+++ App/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void usernameInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void passwordInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
